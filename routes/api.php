d45bd632af4b5d92c1845a3be6b641a3f9cb873d
José Carlos Anaya <j053carlosanach@gmail.com>
2025-04-15 21:35:04 -0600

    Add routes for API
    use php artisan install:api

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\AgentAvailabilityController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::prefix('companies')->group(function () {
+    Route::post('/', [CompanyController::class, 'store']);
+    Route::put('/{company}', [CompanyController::class, 'updateRegisteredAgent']);
+});
+
+Route::get('/agent-availability/{state}', [AgentAvailabilityController::class, 'check']);
